e8192532ae325d1129f69daf70793d7251b28fad
Tyson McNulty <tyson.mcnulty@gmail.com>
2019-03-26 09:05:13 -0400

    Add starting and stopping of a player

--- spec/elevatorMusicSpec.js
+++ spec/elevatorMusicSpec.js
@@ -1,22 +1,38 @@
-const { wrap } = require("../src/ElevatorMusic");
+const ElevatorMusic = require("../src/ElevatorMusic");
 
 describe("elevator music", () => {
+    let player;
+    let wrap;
+
+    beforeEach(() => {
+        player = jasmine.createSpyObj("player", ["start", "stop"]);
+        wrap = new ElevatorMusic(player).wrap;
+    });
+
     const verifyWrapping = (name, returnValue) => describe(`function wrapping: ${name}`, () => {
         let wrappedFunction;
+        let wrapper;
 
         beforeEach(() => {
             wrappedFunction = jasmine.createSpy(name);
-            wrappedFunction.and.returnValue(returnValue);;
+            wrappedFunction.and.returnValue(returnValue);
+
+            wrapper = wrap(wrappedFunction);
         });
 
         it("wraps the provided function", async () => {
-            const wrapper = wrap(wrappedFunction);
-
             const result = await wrapper();
 
             expect(wrappedFunction).toHaveBeenCalled();
             expect(result).toEqual(await returnValue);
         });
+
+        it("starts and stops the player", async () => {
+            await wrapper();
+
+            expect(player.start).toHaveBeenCalled();
+            expect(player.stop).toHaveBeenCalled();
+        })
     });
 
     verifyWrapping("doWorkNow", "done");
--- src/ElevatorMusic.js
+++ src/ElevatorMusic.js
@@ -1,7 +1,19 @@
-const wrap = (fn) => async () => {
-    return await fn();
-};
+class ElevatorMusic {
+    constructor(player) {
+        this.player = player;
+        this.wrap = this.wrap.bind(this);
+    }
 
-module.exports = {
-    wrap,
-};
+    wrap(fn) {
+        const self = this;
+
+        return async () => {
+            self.player.start();
+            const result = await fn();
+            self.player.stop();
+            return result;
+        }
+    }
+}
+
+module.exports = ElevatorMusic;
